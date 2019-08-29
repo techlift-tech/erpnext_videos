b49b8472859c316bb987e351e688a1b9bdcbc638
palashjhabak <palash@techlift.in>
2019-08-30 01:14:36 +0530

    Fixing Selection Bug

--- videos/patient.js
+++ videos/patient.js
@@ -3,18 +3,13 @@ import f from '../lib';
 
 
 fixture("Patient")
-    .page ("http://localhost:8000/login");
+	.page ("http://localhost:8000/login");
 
-test('Patient in ERPNext', async t => {
-	
+test('Patient in ERPNext', async (Selector, t) => {
 	await f.login('administrator','admin')
-
-	await f.use_voice("samantha")
-
-    await f.toggle_speak(true)
-	
-	
-	await f.speak(`
+	await f.use_voice("Microsoft Zira Desktop")
+	await f.toggle_speak(true)
+		await f.speak(`
 
 		Hi.
 
@@ -22,32 +17,22 @@ test('Patient in ERPNext', async t => {
 
 		In this video, we will learn how to created new patient	`)
 	await f.speak(`you can click on patient icon shown on screen or search on global search`)
-	
-    await f.search ("patient li","Patient List")
 
+	await f.search ("patient li","Patient List")
 	await f.speak(`click on new patient`)
-    await f.click_primary_button('New')
-    await f.speak(`fill the following fields respectively`)
-
-    await f.fill_field("Full Name","suraj")
-  
-    await f.fill_field("Mobile","8550902230")
-    await f.fill_field("dob","25/09/1996")
-    
+	await f.click_primary_button('New')
+	await f.speak(`fill the following fields respectively`)
+	await f.fill_field("Full Name","suraj")
+	await f.fill_field("Mobile","8550902230")
+	await f.fill_field("dob","25/09/1996")
 	await f.fill_field("Gender","Male")
-
-	await f.fill_field("Blood Group","b+")
-
-	
+	await f.fill_field("Blood Group","B Positive")
 	await f.speak(`now click on save buttom and then submit`)
 	await f.click_primary_button("Save")
 	await f.click_primary_button("Submit")
 	await f.click_primary_button("Yes")
-await f.speak(`
+	await f.speak(`
 			Hope you found this video tutorial useful.
 			Thanks for watching.
 		`)
-	
-		
-	
 });
