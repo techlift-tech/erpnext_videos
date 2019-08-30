5257cae77c43603dc7faf7ccff180d867ee748c5
suraj varade <suraj@techlift.in>
2019-08-30 23:30:12 +0530

    hello

--- videos/patient.js
+++ videos/patient.js
@@ -9,30 +9,30 @@ test('Patient in ERPNext', async (Selector, t) => {
 	await f.login('administrator','admin')
 	await f.use_voice("Microsoft Zira Desktop")
 	await f.toggle_speak(true)
-		await f.speak(`
+	
+	await f.click_icon('Patient')
 
-		Hi.
-
-		Welcome to E R P Next tutorial.
-
-		In this video, we will learn how to created new patient	`)
-	await f.speak(`you can click on patient icon shown on screen or search on global search`)
-
-	await f.search ("patient li","Patient List")
-	await f.speak(`click on new patient`)
 	await f.click_primary_button('New')
-	await f.speak(`fill the following fields respectively`)
+	await f.close_modal()
+	await f.goto_desk()
+
+	await f.search ("new patient ","New Patient")
+	
+	
+
 	await f.fill_field("Full Name","suraj")
+
 	await f.fill_field("Mobile","8550902230")
+	
 	await f.fill_field("dob","25/09/1996")
+	
 	await f.fill_field("Gender","Male")
+	
 	await f.fill_field("Blood Group","B Positive")
-	await f.speak(`now click on save buttom and then submit`)
+	
+	
 	await f.click_primary_button("Save")
 	await f.click_primary_button("Submit")
 	await f.click_primary_button("Yes")
-	await f.speak(`
-			Hope you found this video tutorial useful.
-			Thanks for watching.
-		`)
+
 });
